d7027cbae8db401024556c8c1c417323a10d7735
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-04-09 19:21:06 +0500

    local-image

--- lambda_container_eith_local_image/lib/lambda_container_eith_local_image-stack.ts
+++ lambda_container_eith_local_image/lib/lambda_container_eith_local_image-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class LambdaContainerEithLocalImageStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const lambdaFn = new lambda.DockerImageFunction(this, "lambdaFunction", {
+      //make sure the lambdaImage folder must container Dockerfile
+      code: lambda.DockerImageCode.fromImageAsset("lambdaImage")
+    });
+
   }
 }
